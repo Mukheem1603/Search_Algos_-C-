51b9a67e3bc1218b05b37bd72f5b76d89f809ee6
Pravallika Kambhampati <50865569+pravzzyy@users.noreply.github.com>
2019-11-06 09:09:36 +0530

    Update BinarySearch.c
    updated the code, as it was not giving the desired output :)

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,85 +1,83 @@
-#include<stdio.h>
-#include<conio.h>
-int BinSearch(int a[],int low,int high,int key)
-{
-	int mid,pos;
-	mid=(low+high)/2;
-	
-while(low<=high)
-	{
-	
-	if(a[mid]==key)
-	{
-		pos=mid;
-	}
- 	else if(key>a[mid])
-	{
-		BinSearch(a,mid+1,high,key);
-	}
-	else
-	{
-		BinSearch(a,low,mid-1,key);
-	}
-	}
-	if(low>high)
-	{
-		pos=-1;
+#include <stdio.h>
 
-	}
-	return pos;
+#define MAX 20
+
+// array of items on which linear search will be conducted. 
+int intArray[MAX] = {1,2,3,4,6,7,9,11,12,14,15,16,17,19,33,34,43,45,55,66};
+
+void printline(int count) {
+   int i;
+	
+   for(i = 0;i <count-1;i++) {
+      printf("=");
+   }
+	
+   printf("=\n");
 }
-void sort(int a[],int n)
-{
-	int i,j,c=0;
-	for(i=0;i<n-1;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]>a[j])
-			{
-			c=a[i];
-			a[i]=a[j];
-			a[j]=c;
-	     	}
-		}
-	}
-}
-void main()
-{
-	int a[100];
-	int n,low=0,high,pos=-1,key,i;
-	printf("Enter number of elements:\n");
-	scanf("%d",&n);
-	high=n-1;
-	printf("Enter the elements:\n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	sort(a,n);
-	printf("The Sorted list is:\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\t",a[i]);
-	}
-	printf("\n");
-	printf("Enter the key element:\n");
-	scanf("%d",&key);
-	if(key>=a[0]&&key<=a[n-1])
-	{
-		pos=BinSearch(a,low,high,key);
-		if(pos!=-1)
-		{
-			printf("Position:%d\nIndex:%d\n",pos+1,pos);
-		}
-		else
-		{
-			printf("Not Found.,%d\n",pos);
-		}
-	}
-      else
-		{
-			printf("Not Found!!\n");
-		}
+
+int find(int data) {
+   int lowerBound = 0;
+   int upperBound = MAX -1;
+   int midPoint = -1;
+   int comparisons = 0;      
+   int index = -1;
+	
+   while(lowerBound <= upperBound) {
+      printf("Comparison %d\n" , (comparisons +1) );
+      printf("lowerBound : %d, intArray[%d] = %d\n",lowerBound,lowerBound,
+         intArray[lowerBound]);
+      printf("upperBound : %d, intArray[%d] = %d\n",upperBound,upperBound,
+         intArray[upperBound]);
+      comparisons++;
 		
+      // compute the mid point
+      // midPoint = (lowerBound + upperBound) / 2;
+      midPoint = lowerBound + (upperBound - lowerBound) / 2;	
+		
+      // data found
+      if(intArray[midPoint] == data) {
+         index = midPoint;
+         break;
+      } else {
+         // if data is larger 
+         if(intArray[midPoint] < data) {
+            // data is in upper half
+            lowerBound = midPoint + 1;
+         }
+         // data is smaller 
+         else {
+            // data is in lower half 
+            upperBound = midPoint -1;
+         }
+      }               
+   }
+   printf("Total comparisons made: %d" , comparisons);
+   return index;
+}
+
+void display() {
+   int i;
+   printf("[");
+	
+   // navigate through all items 
+   for(i = 0;i<MAX;i++) {
+      printf("%d ",intArray[i]);
+   }
+	
+   printf("]\n");
+}
+
+void main() {
+   printf("Input Array: ");
+   display();
+   printline(50);
+	
+   //find location of 1
+   int location = find(55);
+
+   // if element was found 
+   if(location != -1)
+      printf("\nElement found at location: %d" ,(location+1));
+   else
+      printf("\nElement not found.");
 }
